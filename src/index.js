3d9bdca748f027eec34010221ce4f57f699c7b0a
EvgeniyaAnisko <evgeniya.anisko@gmail.com>
2022-01-27 01:53:21 +0300

    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  let temp = str.split('');
+
+  for (let i = 0; i < temp.length; i++) 
+    for (let j = 0; j < bracketsConfig.length; j++){
+      if (temp[i] == bracketsConfig[j][0] && temp[i + 1] == bracketsConfig[j][1]) {
+          temp.splice(i, 2);
+          i = -1;
+          j = -1;
+      }
+  }
+
+  if (temp.length)
+    return false
+    else
+    return true
+
 }
